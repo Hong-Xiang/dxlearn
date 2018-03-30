d90212f59008ca3fd4572fc7e30fc94fc8d75f6b
chengaoyu <chengaoyu@sjtu.edu.cn>
2018-03-30 14:35:04 +0800

    merge remote

--- develop-cgy/run.sh
+++ develop-cgy/run.sh
@@ -1,6 +1,10 @@
-python effmap.py -j master &
-sleep 3
-CUDA_VISIBLE_DEVICES="0" python effmap.py -j worker -t 0 &
-CUDA_VISIBLE_DEVICES="1" python effmap.py -j worker -t 1 &
+# python effmap.py -j master &
+# sleep 3
+# CUDA_VISIBLE_DEVICES="0" python effmap.py -j worker -t 0 &
+# CUDA_VISIBLE_DEVICES="1" python effmap.py -j worker -t 1 &
 #python tor_rec.py -j worker -t 2 &
 #python tor_rec.py -j worker -t 3 &
+CUDA_VISIBLE_DEVICES="0" python scanner.py -r 0 -e 108 &
+CUDA_VISIBLE_DEVICES="1" python scanner.py -r 108 -e 216 &
+CUDA_VISIBLE_DEVICES="0" python scanner.py -r 216 -e 324 &
+CUDA_VISIBLE_DEVICES="1" python scanner.py -r 324 -e 432 &
