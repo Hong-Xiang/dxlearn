497b9d5b282e83539f9f4fca07d0ea3624e5e634
chengaoyu <chengaoyu@sjtu.edu.cn>
2018-04-11 14:48:49 +0800

    clean out the SRF files

--- src/python/dxl/learn/model/tor_recon/__init__.py
+++
@@ -1,4 +0,0 @@
-from .projections import Projection, BackProjection
-from .recon_step import ReconStep
-from .utils import seperate_lors
-from .high_level import recon_step
--- src/python/dxl/learn/tensor/test.py
+++
@@ -1,7 +0,0 @@
-from ..core import Tensor
-
-
-class TensorTest(Tensor):
-    def add_one(self):
-        with self.graph_info.variable_scope():
-            return TensorTest(self.data + 1, self.data_info, self.graph_info.update(name=None))
