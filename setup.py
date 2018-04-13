c7dcb6977a0abcbc67dbb9c26d7774adf8a05c20
HongXiang <hx.hongxiang@gmail.com>
2018-04-13 17:53:45 +0800

    fix package problem

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup, find_packages
 setup(
     name='dxl-learn',
-    version='0.0.6',
+    version='0.0.7',
     description='Machine learn library.',
     url='https://github.com/Hong-Xiang/dxlearn',
     author='Hong Xiang',
     author_email='hx.hongxiang@gmail.com',
     license='MIT',
-    packages=[
-        'dxl.learn',
-        'dxl.learn.core',
-    ],
+    namespace_packages=['dx'],
+    packages=find_packages('src/python'),
     package_dir={'': 'src/python'},
     install_requires=['dxl-fs', 'click', 'dxl-shape'],
     zip_safe=False)
