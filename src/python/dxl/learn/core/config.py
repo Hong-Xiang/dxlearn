67b9714eab89ad95fd1804d3cee976e01baeabe4
chengaoyu <chengaoyu@sjtu.edu.cn>
2018-04-20 18:35:54 +0800

    add some necessary files

--- src/python/dxl/learn/core/config.py
+++ src/python/dxl/learn/core/config.py
@@ -33,9 +33,11 @@ class Configurable(metaclass=ABCMeta):
 
 
 class ConfigurableWithName(Configurable):
-    def __init__(self, name: Path, config: Dict[str, 'Config']):
+    def __init__(self, name: Path, config: Dict[str, 'Config'] = None):
         super().__init__()
         # if config is not none, update it.
+        if config is None:
+            config = {}
         for key, value in config.items():
             if value:
                 self._config.update({key: value})
