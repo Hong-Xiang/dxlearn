94fd660754b6cc5baa225d78326868e9c723e244
HongXiang <hx.hongxiang@gmail.com>
2018-04-24 11:03:28 +0800

    prepare for merge

--- src/python/dxl/learn/core/__init__.py
+++ src/python/dxl/learn/core/__init__.py
@@ -1,5 +1,6 @@
 from .config import ConfigurableWithName, ConfigurableWithClass
 from .distribute import make_distribute_host, Master, ThisHost, Host, Server, Barrier
+from .distribute import ClusterSpec
 from .distribute_task import DistributeTask
 from .session import make_distribute_session, make_session, ThisSession, Session
 from .graph_info import GraphInfo, DistributeGraphInfo
