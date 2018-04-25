96aca3ed60e49dfbfc4c9e86e2582abc0deab046
HongXiang <hx.hongxiang@gmail.com>
2018-04-25 19:40:50 +0800

    add vs ignore

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='dxl-learn',
-    version='0.0.8',
+    version='0.0.9',
     description='Machine learn library.',
     url='https://github.com/Hong-Xiang/dxlearn',
     author='Hong Xiang',
