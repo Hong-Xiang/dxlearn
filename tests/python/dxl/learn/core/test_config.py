9436bc068d3bb4eb5e09324593053df1032f97e3
HongXiang <hx.hongxiang@gmail.com>
2018-05-04 11:08:49 +0800

    add default_config support

--- tests/python/dxl/learn/core/test_config.py
+++ tests/python/dxl/learn/core/test_config.py
@@ -4,5 +4,50 @@ import dxl.learn.core.config as dlcc
 
 class TestConfigurableWithName(unittest.TestCase):
     def test_basic(self):
+        dlcc.DefaultConfig.reset()
         c = dlcc.ConfigurableWithName('x', {'a': 1, 'b': 2})
         self.assertEqual(c.config('a'), 1)
+
+    def test_baisc2(self):
+        dlcc.DefaultConfig.reset()
+        c = dlcc.ConfigurableWithName('x/y', {'a': 1, 'b': 2})
+        self.assertEqual(c.config('a'), 1)
+
+    def test_inherent(self):
+        dlcc.DefaultConfig.reset()
+        c0 = dlcc.ConfigurableWithName('x', {'a': 0})
+        self.assertEqual(c0.config('a'), 0)
+        c1 = dlcc.ConfigurableWithName('x/y')
+        self.assertEqual(c1.config('a'), 0)
+
+    def test_default(self):
+        dlcc.DefaultConfig.reset()
+        class A(dlcc.ConfigurableWithName):
+            @classmethod
+            def default_config(cls):
+                return {'a': 1}
+
+        c1 = A('x')
+        self.assertEqual(c1.config('a'), 1)
+        c2 = A('x', {'a': None})
+        self.assertEqual(c2.config('a'), 1)
+        c3 = A('x', {'a': 2})
+        self.assertEqual(c3.config('a'), 2)
+
+    def test_inherent_default(self):
+        dlcc.DefaultConfig.reset()
+        class A(dlcc.ConfigurableWithName):
+            @classmethod
+            def default_config(cls):
+                return {'a': 1}
+
+        c0 = dlcc.ConfigurableWithName('x', {'a': 0})
+        self.assertEqual(c0.config('a'), 0)
+        c1 = A('x/y')
+        self.assertEqual(c1.config('a'), 0)
+        c2 = A('x/y', {'a': None})
+        self.assertEqual(c2.config('a'), 0)
+        c3 = A('x/y', {'a': 2})
+        self.assertEqual(c3.config('a'), 2)
+        c4 = A('z')
+        self.assertEqual(c4.config('a'), 1)
