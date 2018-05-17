15264d01c06ae3bf116048b7f5a95a6480fd8d93
kongkong@swift <jyk104@163.com>
2018-05-17 09:58:09 +0800

    merge upstream

--- tests/python/dxl/learn/dataset/test_manager.py
+++ tests/python/dxl/learn/dataset/test_manager.py
@@ -4,4 +4,4 @@ from dxl.learn.dataset import DatasetManager
 class TestDatasetManager(unittest.TestCase):
     def test_path_datasets(self):
         self.assertEqual(DatasetManager.path_datasets, '/something/wrong?')
-        
+
