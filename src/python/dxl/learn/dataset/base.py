035714c9eeb67108728cefee900db06b32262220
kongkong@swift-win <jyk104@163.com>
2018-05-25 17:56:27 +0800

    start dataset

--- src/python/dxl/learn/dataset/base.py
+++ src/python/dxl/learn/dataset/base.py
@@ -1,33 +1,98 @@
+import tables as tb
 from ..core import Graph
+from dxl.fs import Path
+from typing import Dict
+from .partition import Partition
 
-"""
-A dataset should handle:
-1.  Loading raw data (ndarray or nested ndarrays)
-2.  Partition into different part
-3.  Processing to tf.Tensor or Tensor as interface
-"""
 
 class Dataset(Graph):
+    class KEYS(Graph.KEYS):
+        class TENSOR:
+            pass
+        class CONFIG:
+            pass 
+
     def __init__(self,
                  name,
                  tensors=None,
                  subgraphs=None,
                  info=None,
-                 config=None,
-                 partition=None,
-                 ):
+                 config=None):
         super.__init__(name, tensors, subgraphs, info, config)
+        self.construct(name)
 
-    @property
+    def construct(self, name):
+        self.handel = self.loader(name)
+        self.pre_processing(self.handel)
+        self.partition()
+        self.post_processing()
+   
+    def loader(self, name):
+        return NotImplementedError
+
+    def pre_processing(self, handel):
+        pass 
+    
     def capacity(self):
+        return NotImplementedError
+
+    def partition(self):
         pass
 
+    def post_processing(self):
+        pass 
+        # x = tf.data.Dataset(...)
+        # x = tf.data.make_one_shot_iterator()
+        # x = x.next()
+        # self.tensors['x'] = ...
+   
 
-class HDF5Dataset(Graph):
-    """
-    """
+class HDF5Dataset(Dataset):
+    '''Default pytables
+    '''
+    class KEYS(Dataset.KEYS):
+        class TENSOR:
+            pass 
+        class CONFIG:
+            IN_MEMORY = 'in_memory'
+            FIELD = 'field'
+        class CMD:
+            ITER = 'hand=h5.{}.iterrows'
+        
+    def __init__(self, name, config, info=None):
+        super().__init__(
+            name=name,
+            config=config,
+            info=info)
+    
+    def loader(self, name):
+        with tb.open_file(name, mode="r") as h5:
+            handels = {}
+            field = self.config(self.KEYS.CONFIG.FIELD)
+            for k, v in field.items():
+                hand = []
+                cmd = 'hand=h5.{}.iterrows'.format(v)
+                exec(cmd)
+                
+                if self.config(self.KEYS.CONFIG.IN_MEMORY):
+                    hand = []
+                    cmd = 'hand=[x[{}] for x in h5.{}.iterow]' 
+                else:
+                   
+                handels.update({k : hand})
 
-    def __init__(self, name, ,):
+            return handels
+
+    def pre_processing(self, handel):
         pass
+    
+           
 
+
+class FileDataset(Dataset):
+    pass 
+
+
+class NpyDataset(Dataset):
+    pass
 
