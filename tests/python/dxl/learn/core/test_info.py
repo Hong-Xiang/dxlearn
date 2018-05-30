c89b2265f288533a373f73c2ebd042d60438697e
HongXiang <hx.hongxiang@gmail.com>
2018-05-30 14:41:59 +0800

    info tests passed

--- tests/python/dxl/learn/core/test_info.py
+++ tests/python/dxl/learn/core/test_info.py
@@ -1,5 +1,5 @@
 from dxl.learn.core.graph_info import GraphInfo
-from dxl.learn.utils.test_utils import sandbox
+from dxl.learn.test import TestCase
 import tensorflow as tf
 import unittest
 import re
@@ -14,53 +14,41 @@ from functools import wraps
 #     return inner
 
 
-class TestGraphInfo(unittest.TestCase):
+class TestGraphInfo(TestCase):
     def create_simple_info(self, name='x'):
         return GraphInfo(name, None, False)
 
     def create_reuseable_info(self, name='x'):
         return GraphInfo(name, None, True)
 
-    def setUp(self):
-        tf.reset_default_graph()
-
-    def tearDown(self):
-        tf.reset_default_graph()
-
     def test_construct_tensor_info(self):
         info = self.create_simple_info('x')
-        assert info.name == 'x'
+        self.assertNameEqual(info, 'x')
 
-    def assertNameEqualIfIgnoreColonAndInt(self, expect, result):
-        pass
-
-    @sandbox()
     def test_scope(self):
         info = self.create_simple_info('scope')
         with info.variable_scope():
             x = tf.get_variable('x', [], tf.float32)
-            assert x.name == 'scope/x'
+            self.assertNameEqual(x, 'scope/x')
 
-    @sandbox()
     def test_reuse_scope(self):
         info = self.create_simple_info('scope')
         with info.variable_scope():
             x0 = tf.get_variable('x', [], tf.float32)
         with info.variable_scope(reuse=True):
             x1 = tf.get_variable('x', [], tf.float32)
-        assert x0.name == 'scope/x'
-        assert x1.name == 'scope/x'
+        self.assertNameEqual(x0, 'scope/x')
+        self.assertNameEqual(x1, 'scope/x')
 
-    @sandbox()
     def test_tf_varible_scope_compat(self):
         info = self.create_simple_info('scope')
         with tf.variable_scope('scope0'):
             with info.variable_scope() as scope:
-                assert scope.name == 'scope0/scope'
+                self.assertNameEqual(scope, 'scope0/scope')
 
     def test_update(self):
         info = GraphInfo('x', None, False)
-        info_u = info.update(name=info.name_raw / 'y')
+        info_u = info.update(name=info.name / 'y')
         self.assertEqual(info_u.name, 'x/y')
 
 
