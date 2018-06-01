d7d058ea3db68cc001ea706cdfd339c881437709
HongXiang <hx.hongxiang@gmail.com>
2018-06-01 14:58:53 +0800

    fixed sandbox

--- src/python/dxl/learn/backend/_tensorflow.py
+++ src/python/dxl/learn/backend/_tensorflow.py
@@ -5,19 +5,19 @@ from contextlib import contextmanager
 
 
 class TensorFlow(Backend):
-    def in_sandbox(self, func):
+    def sandbox(self, func):
         @wraps(func)
         def inner(*args, **kwargs):
-            with self.sandbox():
-               return func(*args, **kwargs)
+            with self.sandbox_impl():
+                return func(*args, **kwargs)
 
         return inner
 
     @contextmanager
-    def sandbox(self):
+    def sandbox_impl(self):
         with tf.Graph().as_default():
             yield
- 
+
     def unbox(self):
         return tf
 
