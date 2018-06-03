60ba43e521e1e58f8f06721bfa9074c19d866cdb
HongXiang <hx.hongxiang@gmail.com>
2018-06-03 11:02:01 +0800

    add test for Summation

--- tests/python/dxl/learn/model/test_on_collections.py
+++ tests/python/dxl/learn/model/test_on_collections.py
@@ -5,9 +5,23 @@ from dxl.learn.model import Summation
 
 class TestSummation(TestCase):
     def test_directly_constructed(self):
-        x = Constant(1.0)
-        y = Constant(2.0)
+        x = Constant(1.0, 'x')
+        y = Constant(2.0, 'y')
         s = Summation('summation', [x, y])
         v = s()
         with self.test_session() as sess:
-            pass
+            assert sess.run(v) == 3.0
+
+    def test_summation_of_same_tensor(self):
+        x = Constant(1.0, 'x')
+        s = Summation('summation', [x, x, x])
+        v = s()
+        with self.test_session() as sess:
+            assert sess.run(v) == 3.0
+
+    def test_construct_with_none_input(self):
+        x = Constant(1.0, 'x')
+        s = Summation('summation')
+        v = s([x] * 3)
+        with self.test_session() as sess:
+            assert sess.run(v) == 3.0
