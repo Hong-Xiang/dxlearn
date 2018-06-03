a1797e9bceb8669aaeb5e93ba62c6240133d533d
HongXiang <hx.hongxiang@gmail.com>
2018-06-03 22:32:24 +0800

    add TestSession run list test

---
+++ tests/python/dxl/learn/core/test_session.py
@@ -0,0 +1,9 @@
+from dxl.learn.test import TestCase
+from dxl.learn.core import Constant
+
+
+class TestTestSession(TestCase):
+    def test_run_list(self):
+        c = Constant(1.0, 'const')
+        with self.test_session() as sess:
+            assert sess.run([c, c]) == [1.0, 1.0]
