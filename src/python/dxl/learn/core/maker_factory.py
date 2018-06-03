0f32ddf80d6b6507562f56f8424efebeb9dfb684
HongXiang <hx.hongxiang@gmail.com>
2018-06-03 21:57:14 +0800

    add SubgraphMakerFinder

--- src/python/dxl/learn/core/maker_factory.py
+++ src/python/dxl/learn/core/maker_factory.py
@@ -12,3 +12,17 @@ class SubgraphMakerFactory:
     @classmethod
     def reset(cls):
         cls._graph_maker_builders = {}
+
+
+class SubgraphMakerFinder:
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+
+    def __call__(self, father_graph, subgraph_name):
+        result = SubgraphMakerFactory.get(
+            father_graph.info.name / subgraph_name)
+        if len(self._args) == 0 and len(self._kwargs) == 0:
+            return result(father_graph, subgraph_name)
+        else:
+            return result(*args, **kwargs)(father_graph, subgraph_name)
