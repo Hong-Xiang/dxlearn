15887c77fe58b350ea0021cd0579c4c88d74716f
kongkong@swift-win <jyk104@163.com>
2018-06-12 09:21:30 +0800

    update graph __doc__

--- src/python/dxl/learn/core/graph.py
+++ src/python/dxl/learn/core/graph.py
@@ -11,35 +11,38 @@ from .subgraph_maker import SubgraphPartialMaker, SubgraphMaker, SubgraphMakerTa
 
 class Graph(ConfigurableWithName):
     """
-    Graph is a collections of Tensors, subgraphs and configs.
+    Graph is a collections of Tensors, graphs, configs and info.
 
     Features for config:
 
-    - provide access to default / external config by info.name;
+    - provide access to default / external config by `info.name`;
+    - provide self.config(key) search-like config getter, from config of father graph.
     - provide _default_config() classmethod;
-    - provide self.config(key) rearch-like config getter.
 
 
     Features for tenosrs:
-    - provide default collections of holding tensors: self.tensors
-    - access to these 
+    - provide default collections of holding tensors: self.tensors, main interface of graph.
 
     `self.tensors` is a dict of Tensors, which is provided as an interface to Graph.
     Which means one may use ::
 
         g = Graph(...)
-        g.run(key)
-
+        g.run(key) 
+        sess.run(g.tensor(key))
+        
     to run corresponding tensors.
 
     Another useage is to substitute part of graph with other tensors. One may use ::
 
-        g = Graph(tensor_dict, ...)
-        g.run(key, feeds={k: tensor})
+        g = SomeGraph(tensor_dict, ...)
+        g.tensors = {'x': Tensor}
+        g.run(key, feeds={'x': tensor, 'y': tensor2})
+        g.x == g.tensor('x')
+        g.run(key, feeds={g.x: tensor, g.y: tensor})
 
     which is equivilant to ::
 
-        tf.run(g.tensor(key).data, feeds={g.tensor[k]:tensor})
+        sess.run(g.tensor(key).data, feeds={g.tensor(k).data:tensor for k in ['x', 'y']})
 
 
     KEYS:
@@ -48,98 +51,65 @@ class Graph(ConfigurableWithName):
         - `MAIN`: Main purpose/effect of graph, thus the one which is fetched by
         by default, thus `g.run()` is eqvilant to `g.run(g.KEYS.TENSOR.MAIN)`.
 
-      Methods:
-
-      Support using Graph as an "function" on Tensors.
-
-
-
-
-
-      - tensor(key): -> Tensor
+        - tensor(key): -> Tensor
 
 
     Provide The following methods:
 
     - `g.tensor(key)`
-    - `g.subgraph(key)`
+    - `g.graph(key)`
     - `g.config(key)`
 
+    - `g.run(key)`
 
-    #### Old subgraph maker design
 
-    Since our library targeting easily reuse and substibution of subgraph,
-    there would be four common cases when constructing Graph with subgraphs.
+    # graph maker design
+
+    Since our library targeting easily reuse and substitution of sub-graph,
+    there would be four common cases when constructing Graph with sub-graphs.
 
     1. father graph is not going to be reused (e.g. for Graphs), subgraph is fixed
     2. father graph is going to be reused (e.g. for Model), subgraph is fixed
     3. father graph is not going to be reused, subgraph is configurable
     4. father graph is going to be reused, subgraph is configurable
-    
+
     For case 1:
     just directly code it in kernel:
-    ```
+    ``` python
     def kernel(self):
         x = self.tensor('input')
         subg = SomeGraph(self.info.child_scope('sub'), tensors={'x': x})
+        subg.make()
         y = subg.tensor('y')
     ```
 
     For case 2:
-    Use `subgraphs` collection.
-    ```
+    Use `graphs` collection.
+    ``` python
+    # subg is model
     def kernel(self):
         x = self.tensor('input')
-        subg = self.subgraph('sub', lambda g, n: SomeModel(g.info.child_scope('sub'), inputs={'x': x}))
-        y = subg()
+        subg = self.graph('sub', Conv2D(filters=32))
+        y = subg(x)
     ```
 
     For case 3:
-    Use `SubgraphMakerFactory'
-    ```
+    ``` python
     def kernel(self):
         x = self.tensor('input')
-        subg = SubgraphMakerFactory.get(self.info.name/'sub')(x=x)(self, 'sub')
+        subg = self.graph('sub')
+        subg.tensors['x'] = x
+        subg.make()
         y = subg.tensor('y')
-    ```
-    Or, with short access
-    ```
-    subg = self.make_subgraph('sub')(x=x)
     ```
 
     For case 4:
-    Use `SubgraphMakerFactory` with `subgraphs` collection.
-    ```
+    ``` python
     def kernel(self):
         x = self.tensor('input')
-        subg = self.subgraph('sub', SubgraphMakerFactory.get(self.info.name/'sub')(x=x))
-        y = subg()
+        subg = self.graph('sub')
+        y = subg(x)
     ```
-    Or, with short access
-    ```
-    subg = self.subgraph('sub', self.subgraph_maker('sub')(x=x))
-    ```
-    
-    When rigister a SubgraphMakerBuilder, you may use
-    ```
-    SubgraphMakerFactory.registe('g/subg', SomeGraph.maker_builder)
-    ```
-    
-    As a summary, we have three order of using subgraphs:
-    1.  Graph object, direct/hard-coded in kernel.
-    2.  Graph maker function (SubgraphMaker), with fixed signature: Callable[[Graph, str], Graph]
-    3.  Graph maker builder function, with arbitary arguments: Callable[[*args, **kwargs], SubgraphMaker]
-
-    #### subgraph maker design 0605
-    Search:
-    1. self.subgraphs
-    2. SubgraphMakerTable (should we change name to SubgraphTable or something else?)
-    3. maker
-
-    Construction:
-    SubgraphMaker(type(Graph) or GraphTypeMaker,  SubgraphPartialMaker) :: SubgraphMaker.__call__() -> Graph
-    SubgraphPartialMaker(*args, **kwargs) :: SubgraphPartialMaker(type) -> Graph
-    GraphTypeMaker :: GraphTypeMaker.__call__() -> type of Graph (constructor?)
     """
 
     class KEYS:
