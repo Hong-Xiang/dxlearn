1a2900d8c8a05fa40fea4305ccf14c803e311235
kongkong@swift-win <jyk104@163.com>
2018-06-13 14:48:15 +0800

    fix tensors api

--- src/python/dxl/learn/network/trainer/trainer.py
+++ src/python/dxl/learn/network/trainer/trainer.py
@@ -21,20 +21,20 @@ class Trainer(Graph):
     def kernel(self):
         KT, KS = self.KEYS.TENSOR, self.KEYS.SUBGRAPH
         self.tensors[KT.TRAIN_STEP] = self.graphs(KS.OPTIMIZER).minimize(
-            self.tensor(KT.OBJECTIVE))
+            self.tensors[KT.OBJECTIVE])
         self.tensors[KT.MAIN] = self.train_step
 
     @property
     def objective(self):
-        return self.tensor(self.KEYS.TENSOR.OBJECTIVE)
+        return self.tensors[self.KEYS.TENSOR.OBJECTIVE]
 
     @property
     def train_step(self):
-        return self.tensor(self.KEYS.TENSOR.TRAIN_STEP)
+        return self.tensors[self.KEYS.TENSOR.TRAIN_STEP]
 
     def learning_rate(self):
-        return self.graphs(self.KEYS.SUBGRAPH.OPTIMIZER).learning_rate
+        return self.graphs[self.KEYS.SUBGRAPH.OPTIMIZER].learning_rate
 
     @property
     def decay_learning_rate(self, step=1):
-        return self.graphs(self.KEYS.SUBGRAPH.OPTIMIZER).decay_learning_rate
+        return self.graphs[self.KEYS.SUBGRAPH.OPTIMIZER].decay_learning_rate
