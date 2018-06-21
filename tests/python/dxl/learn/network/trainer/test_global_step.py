a7732582654cf2ac12c3228773ad520027c23021
kongkong@swift-win <jyk104@163.com>
2018-06-21 17:31:14 +0800

    fix test_global_step

--- tests/python/dxl/learn/network/trainer/test_global_step.py
+++ tests/python/dxl/learn/network/trainer/test_global_step.py
@@ -9,13 +9,13 @@ class TestGlobalStep(TestCase):
         return GlobalStep()
 
     def imitate_train(self, g_step):
-        g_step.increased()
+        return g_step.increased()
 
     def test_global_step(self):
         train_step = 5
         g_step = self.make_global_step()
         with self.variables_initialized_test_session() as sess:
             for i in range(train_step):
-                self.imitate_train(g_step)
+                g_step = self.imitate_train(g_step)
                 gt = sess.run(g_step)
-                self.assertEqual(gt, 0)
+                self.assertEqual(gt, i)
