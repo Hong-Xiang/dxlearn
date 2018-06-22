303bcde76557577089025ae1296e00686a0b25cd
kongkong@swift-win <jyk104@163.com>
2018-06-22 14:03:36 +0800

    add dense

--- src/python/dxl/learn/model/dense/dense.py
+++ src/python/dxl/learn/model/dense/dense.py
@@ -1,7 +1,63 @@
-from ...core import Model
+import tensorflow as tf
+from dxl.learn.core import Model
 
 __all__ = ['Dense']
 
 
 class Dense(Model):
-    pass
+    class KEYS(Model.KEYS):
+        class CONFIG:
+            N_UNITS = 'n_units'
+            ACTIVATION = 'activation'
+            W_INIT = 'w_init'
+            B_INIT = 'b_init'
+
+    def __init__(self,
+                 info='dense',
+                 inputs=None,
+                 n_units=None,
+                 activation=None,
+                 w_init=None,
+                 b_init=None):
+        KC = self.KEYS.CONFIG
+        super().__init__(
+            info,
+            tensors={self.KEYS.TENSOR.INPUT: inputs},
+            config={
+                KC.N_UNITS: n_units,
+                KC.ACTIVATION: activation,
+                KC.W_INIT: w_init,
+                KC.B_INIT: b_init
+            }
+        )
+    
+    @classmethod
+    def _default_config(cls):
+        KC = cls.KEYS.CONFIG
+        return {
+            KC.N_UNITS: 64,
+            KC.W_INIT: tf.truncated_normal_initializer(stddev=0.1),
+            KC.B_INIT: tf.constant_initializer(value=0.0)
+        }
+
+    def kernel(self, inputs):
+        KT, KC = self.KEYS.TENSOR, self.KEYS.CONFIG
+        x = inputs[KT.INPUT]
+        if x.shape.ndims != 2:
+            raise AssertionError("The input dimension must be rank 2")
+        
+        n_in = x.shape.as_list()[-1]
+        with tf.variable_scope("init_w_b"):
+            w = tf.get_variable(name='w', 
+                                shape=(n_in, self.config(KC.N_UNITS)),
+                                initializer=self.config(KC.W_INIT),
+                                dtype=x.dtype)
+            y = tf.matmul(x, w)
+            if self.config(KC.B_INIT):
+                b = tf.get_variable(name='b',
+                                    shape=(KC.N_UNITS,),
+                                    initializer=self.config(KC.B_INIT),
+                                    dtype=x.dtype)
+            y = tf.nn.bias_add(y, b, name='bias_add')
+
+        return y
