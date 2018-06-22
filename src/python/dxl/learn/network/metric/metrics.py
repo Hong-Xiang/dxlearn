558bfc96d9e3190ad5d44df0588a5e63da7c6156
kongkong@swift-win <jyk104@163.com>
2018-06-22 19:42:14 +0800

    fix metrics

--- src/python/dxl/learn/network/metric/metrics.py
+++ src/python/dxl/learn/network/metric/metrics.py
@@ -2,5 +2,4 @@ import tensorflow as tf
 
 def mean_square_error(label, data):
     with tf.name_scope('mse'):
-        mse, update_op = tf.metrics.mean_squared_error(label, data)
-        return mse
+        return tf.metrics.mean_squared_error(label, data)
