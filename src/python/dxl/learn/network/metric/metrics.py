f8cda62c2016f687df88e4cc0e918a8958467de3
kongkong@swift-win <jyk104@163.com>
2018-07-06 13:46:45 +0800

    add loss

--- src/python/dxl/learn/network/metric/metrics.py
+++ src/python/dxl/learn/network/metric/metrics.py
@@ -1,6 +1,15 @@
 import tensorflow as tf
+from dxl.learn.core import Graph
 
+class Metrics(Graph):
+    class KEYS(Graph.KEYS):
+        class TENSOR(Graph.KEYS.TENSOR):
+            LABEL = 'label'
+            DATA = 'data'
+        
+        class Graph(Graph.KEYS.GRAPH):
+            METRIC = 'metric'
 
-def mean_square_error(label, data):
-    with tf.name_scope('mse'):
-        return tf.losses.mean_squared_error(label, data)
+    def kernel(self, inputs):
+        KT = self.KEYS.TENSOR
+
