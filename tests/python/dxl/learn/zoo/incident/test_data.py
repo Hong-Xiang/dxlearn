2a10fc6faadeca0bcf60e6aa034274a52d8960da
HongXiang <hx.hongxiang@gmail.com>
2018-07-06 23:00:21 +0800

    add SplitByPeriod

--- tests/python/dxl/learn/zoo/incident/test_data.py
+++ tests/python/dxl/learn/zoo/incident/test_data.py
@@ -1,6 +1,6 @@
 from dxl.learn.test import TestCase
 from dxl.data.zoo.incident import load_table, Hit, Photon, Coincidence
-from dxl.learn.zoo.incident.data import reindex_crystal, binary_crystal_index
+from dxl.learn.zoo.incident.data import reindex_crystal, binary_crystal_index, SplitByPeriod
 
 # from dxl.learn.zoo.incident.data import create_dataset, dataset_db
 
@@ -33,3 +33,10 @@ def test_binary_crystal_index():
     assert p2.hits[1].crystal_index == 0
     assert p2.hits[2].crystal_index == 1
     assert p2.hits[3].crystal_index == 0
+
+
+def test_spilit_by_period():
+    f = SplitByPeriod(5, [2, 3])
+    fst, snd = f(list(range(10)))
+    assert fst == [2, 3, 7, 8]
+    assert snd == [0, 1, 4, 5, 6, 9]
