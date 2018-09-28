fa4a0dc479bc8b23217e2234df78ff861a5324ae
hongjiang <hongjiangdong@outlook.com>
2018-09-28 16:46:21 +0800

    new version update

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
 setup(
     name='dxl-learn',
-    version='0.1.3',
+    version='0.2.0',
     description='Machine learn library.',
-    url='https://github.com/Hong-Xiang/dxlearn',
+    url='https://github.com/tech-pi/dxlearn',
     author='Hong Xiang',
     author_email='hx.hongxiang@gmail.com',
     license='MIT',
     namespace_packages=['dxl'],
     packages=find_packages('src/python'),
     package_dir={'': 'src/python'},
-    install_requires=['dxl-fs', 'click', 'dxl-shape',
-                      'dxl-core',
+    install_requires=['jfs', 'click', 'dxl-shape>=0.1.1',
+                      'dxl-core>=0.1.6',
+                      'doufo>=0.0.3'
                       'arrow', 'tqdm'],
     entry_points="""
         [console_scripts]
